75fbdb49a7e957c8a90e70f2f143b2fe4e6f460d
Lathika24 <147816038+Lathika24@users.noreply.github.com>
2024-02-05 15:45:18 +0530

    Create insertionsort.c

---
+++ insertion _sort.c
@@ -0,0 +1,42 @@
+// C program for insertion sort
+#include <math.h>
+#include <stdio.h>
+
+/* Function to sort an array 
+using insertion sort*/
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++) 
+	{
+		key = arr[i];
+		j = i - 1;
+
+		//swapping
+		while (j >= 0 && arr[j] > key) 
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+int main()
+{
+	int n;
+	printf("Enter no. of elements: ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter array elements: ");
+	for(int i=0; i<n;i++) {
+		scanf("%d",&arr[i]);
+	}
+	insertionSort(arr, n);
+	printf("Sorted array is: ");
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+
+	return 0;
+}
